74451821811c80a271454b299f7f84d73c2af905
HypericatsFramework <charlesolivier.malo@gmail.com>
2024-11-18 12:43:48 -0500

    work on window

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window *window = SDL_CreateWindow(
-      "SDL2Test",
+      "Sadge",
       SDL_WINDOWPOS_UNDEFINED,
       SDL_WINDOWPOS_UNDEFINED,
       640,
